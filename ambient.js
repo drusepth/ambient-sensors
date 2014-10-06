15f3faed66fbbaa2787e3e2f8e2432721f2ffdf3
Andrew Brown <drusepth@gmail.com>
2014-10-05 21:15:37 -0500

    Turn LEDs on/off for sound/light activity

--- ambient.js
+++ ambient.js
@@ -1,55 +1,54 @@
 var tessel = require('tessel');
 var ambientlib = require('ambient-attx4');
 
-var light_trigger_level = 0.04;
+// Config
+var light_trigger_level = 0.06;
 var sound_trigger_level = 0.05;
+var poll_time = 500; // ms
+var led_light_time = 500; // ms
+
+// LED references
+var leds = [
+  tessel.led[0].output(0),
+  tessel.led[1].output(0)
+];
+var light_led = leds[0],
+    sound_led = leds[1];
 
 var ambient = ambientlib.use(tessel.port['A']);
 ambient.on('ready', function () {
- // Get points of light and sound data.
-  setInterval( function () {
-    ambient.getLightLevel( function(err, ldata) {
-      if (err) throw err;
-      ambient.getSoundLevel( function(err, sdata) {
-        if (err) throw err;
-        console.log("Light level:", ldata.toFixed(8), " ", "Sound Level:", sdata.toFixed(8));
-      });
-    })
-  }, 500); // The readings will happen every .5 seconds unless the trigger is hit
+  // Display current of light and sound data
+  setInterval(function () { display_current_levels() }, poll_time);
 
   ambient.setLightTrigger(light_trigger_level);
+  ambient.setSoundTrigger(sound_trigger_level);
 
-  // Set a light level trigger
-  // The trigger is a float between 0 and 1
   ambient.on('light-trigger', function(data) {
-    console.log("Our light trigger was hit:", data);
+    // Toggle the LED on
+    light_led.toggle();
 
     // Clear the trigger so it stops firing
     ambient.clearLightTrigger();
-    //After 1.5 seconds reset light trigger
+
+    // Turn the LED back off after some duration and re-set the trigger
     setTimeout(function () {
-
-        ambient.setLightTrigger(light_trigger_level);
-
-    }, 1500);
+      light_led.toggle();
+      ambient.setLightTrigger(light_trigger_level);
+    }, led_light_time);
   });
 
-  // Set a sound level trigger
-  // The trigger is a float between 0 and 1
-  ambient.setSoundTrigger(sound_trigger_level);
-
   ambient.on('sound-trigger', function(data) {
-    console.log("Something happened with sound: ", data);
+    // Toggle the LED on
+    sound_led.toggle();
 
-    // Clear it
+    // Clear the trigger so it stops firing
     ambient.clearSoundTrigger();
 
-    //After 1.5 seconds reset sound trigger
+    // Turn the LED back off after some duration and re-set the trigger
     setTimeout(function () {
-
-        ambient.setSoundTrigger(sound_trigger_level);
-
-    }, 1500);
+      sound_led.toggle();
+      ambient.setSoundTrigger(sound_trigger_level);
+    }, led_light_time);
 
   });
 });
@@ -57,3 +56,14 @@ ambient.on('ready', function () {
 ambient.on('error', function (err) {
   console.log(err)
 });
+
+function display_current_levels() {
+  ambient.getLightLevel(function(err, light_data) {
+    if (err) throw err;
+
+    ambient.getSoundLevel(function(err, sound_data) {
+      if (err) throw err;
+      console.log("Light level:", light_data.toFixed(8), " ", "Sound Level:", sound_data.toFixed(8));
+    });
+  })
+}
